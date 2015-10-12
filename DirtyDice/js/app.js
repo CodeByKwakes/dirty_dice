df7153092a8de38f6561066a2f9c9acba017bfd9
KwakesProject <kwakes@me.com>
2015-10-12 15:49:42 +0100

    got rounds appear now time to get total amount and winner

--- DirtyDice/js/app.js
+++ DirtyDice/js/app.js
@@ -7,6 +7,7 @@ var playerOneRolls = []
 var playerTwoTotal = 0
 var playerTwoRolls = []
 var turn           = 1
+var roundNum       = 1
 
 function setUp(){
   whosTurn.innerHTML = "Player One's turn"
@@ -24,25 +25,29 @@ function rollDice(){
   var d2        = Math.floor(Math.random() * 6) + 1;
   var diceTotal = d1 + d2;
 
+
   if(playerOneRolls.length === 0 || playerOneRolls.length % 3 !== 0 || playerOneRolls.length === playerTwoRolls.length){
+    // player 1
     die1.innerHTML = d1;
     die2.innerHTML = d2;
     playerOneRolls.push([d1, d2])
     playerOneTotal += diceTotal
-    console.log(playerOneRolls);
+    // console.log(playerOneRolls);
     $("#playerOneRolls").append("<li>Throw "+playerOneRolls.length+": "+d1 + " & "+ d2+"</li>")
     status.innerHTML = "Player One Rolled " +d1+ " & " +d2;
     play1.innerHTML = playerOneTotal;
     
     if(d1 == d2){
-      status.innerHTML += " - You rolled DOUBLES! ";
+      status.innerHTML += " - You threw DOUBLES! ";
     }
     
-    if(playerOneRolls.length % 3 === 0){
+    if(playerOneRolls.length % 3 === 0){  //cange this to  == 3
       whosTurn.innerHTML = "Player Two's turn"
+      $('#playerOneRoundCol').append("<li>Round "+ roundNum +": " + playerOneTotal + "</li>")
     }
-  
+
   }else{
+    //player 2
     die1.innerHTML = d1;
     die2.innerHTML = d2;
     playerTwoRolls.push([d1, d2])
@@ -51,17 +56,34 @@ function rollDice(){
     $("#playerTwoRolls").append("<li>Throw "+playerTwoRolls.length+": "+d1 + " & "+ d2+"</li>")
     status.innerHTML = "Player Two Rolled " +d1+ " & " +d2;
     play2.innerHTML = playerTwoTotal;
+    
     if(d1 == d2){
-      status.innerHTML += " - You rolled DOUBLES! ";
+      status.innerHTML += " - You threw DOUBLES! ";
     }
     if(playerTwoRolls.length % 3 === 0){
       whosTurn.innerHTML = "Player One's turn"
+      $('#playerTwoRoundCol').append("<li>Round "+ roundNum +": " + playerTwoTotal + "</li>")
+      roundNum++
+      playerOneTotal = 0
+      playerTwoTotal = 0
+      $('#playerOneRolls').html("")
+      $('#playerTwoRolls').html("")
     }
   }
 }
+
+// function playerRoundScore(){
+//   var playerOneScore = new playerOneRound;
+//   var playerTwoScore = new playerTwoRound;
+
+//   $('#playerOneRound').append("<li>" + playerOneTotal + "</li>")
+
+// }
+
+
+
 //build a function to store the results of each round
 // player one rolls 3 times move from playerOneRolls to playerOneRound
-// $('#playerOneRound').append("<li>" + playerOneTotal + "</li>")
 
 // player two rolls 3 times move from playerTwoRolls to playerOneRound
 // $('#playerTwoRound').append("<li>" + playerTwoTotal + "</li>")
