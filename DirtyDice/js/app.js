53706bab3be90cfaefd18207bd618b93fc41f3ca
KwakesProject <kwakes@me.com>
2015-10-12 13:06:55 +0100

    improved on details. need to get a rounds update function

--- DirtyDice/js/app.js
+++ DirtyDice/js/app.js
@@ -8,19 +8,20 @@ var playerTwoTotal = 0
 var playerTwoRolls = []
 var turn           = 1
 
-
 function setUp(){
-  whosTurn.innerHTML = "Player one's turn"
+  whosTurn.innerHTML = "Player One's turn"
   document.getElementById('diceRoll').addEventListener("click", rollDice);
 }
 
 function rollDice(){
-  var whosTurn = document.getElementById("whosTurn")
-  var die1 = document.getElementById("die1");
-  var die2 = document.getElementById("die2");
-  var status = document.getElementById("status");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
+  var whosTurn  = document.getElementById("whosTurn")
+  var die1      = document.getElementById("die1");
+  var die2      = document.getElementById("die2");
+  var status    = document.getElementById("status");
+  var play1     = document.getElementById("p1Total");
+  var play2     = document.getElementById("p2Total");
+  var d1        = Math.floor(Math.random() * 6) + 1;
+  var d2        = Math.floor(Math.random() * 6) + 1;
   var diceTotal = d1 + d2;
 
   if(playerOneRolls.length === 0 || playerOneRolls.length % 3 !== 0 || playerOneRolls.length === playerTwoRolls.length){
@@ -29,13 +30,14 @@ function rollDice(){
     playerOneRolls.push([d1, d2])
     playerOneTotal += diceTotal
     console.log(playerOneRolls)
-    $("#playerOneRolls").append("<li>"+d1 + ", "+ d2+"</li>")
-    status.innerHTML = "Player one rolled "+playerOneTotal+".";
-    if(d1 == d2){
-      status.innerHTML += " DOUBLES! "+playerOneTotal+".";
-    }
+    $("#playerOneRolls").append("<li>Throw "+playerOneRolls.length+": "+d1 + " & "+ d2+"</li>")
+    status.innerHTML = "Player One Rolled " +d1+ " & " +d2;
+    play1.innerHTML = playerOneTotal;
+    // if(d1 == d2){
+    //   status.innerHTML += " DOUBLES! "+playerOneTotal+".";
+    // }
     if(playerOneRolls.length % 3 === 0){
-      whosTurn.innerHTML = "Player two's turn"
+      whosTurn.innerHTML = "Player Two's turn"
     }
   }else{
     die1.innerHTML = d1;
@@ -43,18 +45,30 @@ function rollDice(){
     playerTwoRolls.push([d1, d2])
     playerTwoTotal += diceTotal
     console.log(playerTwoRolls)
-    $("#playerTwoRolls").append("<li>"+d1 + ", "+ d2+"</li>")
-    status.innerHTML = "Player two rolled "+playerTwoTotal+".";
-    if(d1 == d2){
-      status.innerHTML += " DOUBLES! "+playerTwoTotal+".";
-    }
+    $("#playerTwoRolls").append("<li>Throw "+playerTwoRolls.length+": "+d1 + " & "+ d2+"</li>")
+    status.innerHTML = "Player Two Rolled " +d1+ " & " +d2;
+    play2.innerHTML = playerTwoTotal;
+    // if(d1 == d2){
+    //   status.innerHTML += " DOUBLES! "+playerTwoTotal+".";
+    // }
     if(playerTwoRolls.length % 3 === 0){
-      whosTurn.innerHTML = "Player ones's turn"
+      whosTurn.innerHTML = "Player One's turn"
     }
   }
-
 }
+//build a function to store the results of each round
+// player one rolls 3 times move from playerOneRolls to playerOneRound
+// $('#playerOneRound').append("<li>" + playerOneTotal + "</li>")
 
+// player two rolls 3 times move from playerTwoRolls to playerOneRound
+// $('#playerTwoRound').append("<li>" + playerTwoTotal + "</li>")
 
+// function roundResults(){
+//   var playerOneRoundTotal = 
+
+//   if playerOneRoundTotal
+//   $('#playerOneRound').append("<li>" + playerOneRoundTotal + "</li>")
+//   $('#playerTwoRound').append("<li>" + playerTwoTotal + "</li>")
+// }
 
 
